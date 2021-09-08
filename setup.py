3be177017f4a877562d7a8445e61253d3ec4fb4c
crayyy_zee <zaidlastid20@gmail.com>
2021-09-08 21:54:29 +0500

    fixing package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='amari',
-    version='1.2.0',    
+    version='1.2.1',    
     description='An asynchronous AmariBot API wrapper.',
     url='https://github.com/i-am-zaidali/AmariWrapper',
     author='Zaid Ali',
