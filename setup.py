90b59d6784e68b80ef1e12d00a7c33a8231b34b8
crayyy_zee <zaidlastid20@gmail.com>
2021-09-08 22:18:37 +0500

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='amari',
     version='1.2.1',    
     description='An asynchronous AmariBot API wrapper.',
-    url='https://github.com/i-am-zaidali/AmariWrapper',
+    url='git@github.com:i-am-zaidali/AmariWrapper.git',
     author='Zaid Ali',
     author_email='zaidlastid20@gmail.com',
     install_requires=['aiohttp', 'cachetools', 'requests']
